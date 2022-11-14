7943f665721821615a6fd574f009f40a564bc09a
shcemeleva0 <117357397+shcemeleva0@users.noreply.github.com>
2022-11-14 09:28:50 +0300

    Add files via upload

---
+++ 1.pas
@@ -0,0 +1,16 @@
+﻿begin
+var m:array[1..5] of integer;
+  println('Введите элементы массива');
+  for var i:=1 to 5 do
+  read(m[i]);
+  println;
+  for var i:=1 to 5-1 do
+    begin
+      if m[i]>m[i+1] then
+      begin
+      println('Массив не возрастающий');
+      halt;
+      end;
+    end;
+  println('Массив возрастающий');
+end.
